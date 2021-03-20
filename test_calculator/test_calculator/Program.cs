08d779fe98c1bb6822f7a06dd0c873eaeaefb27e
OLEG <80538246+Oleg5750298@users.noreply.github.com>
2021-03-20 18:03:10 +0300

    Написана консольная программа, которая выводит пользователю сообщение "Введите номер операции: 1.Сложение 2.Вычитание 3.Умножение". Рядом с названием каждой операции указан ее номер, например, операция вычитания имеет номер 2. Пусть пользователь вводит в программу номер операции, и в зависимости от номера операции программа выводит ему название операции.

--- test_calculator/test_calculator/Program.cs
+++ test_calculator/test_calculator/Program.cs
@@ -6,7 +6,40 @@ namespace test_calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите первое число: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите знак операции: + Сложение  - Вычитание  * Умножение / Деление" );
+
+            var operation = Console.ReadKey().KeyChar;
+            double result = 0;
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    Console.WriteLine($"Результат операции {result}");
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    Console.WriteLine($"Результат операции {result}");
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    Console.WriteLine($"Результат операции {result}");
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    Console.WriteLine($"Результат операции {result}");
+                    break;
+                default:
+                    Console.WriteLine("Неизвестная операция");
+                    break;
+
+            }
+            Console.ReadKey();
+
         }
     }
 }
